56e9f8d3fc43eaca35a4f409884240c799d25bd3
Jirapongs55 <nut36.jirapong@hotmail.com>
2021-07-08 19:52:41 +0700

    Create Exercise4_Jirapong_S.py

---
+++ Exercise4_Jirapong_S.py
@@ -0,0 +1,11 @@
+feScore = 90
+gbScore = 56.5
+icsScore = 82
+cpScore = 99.95
+
+print("-"*3,"Your Score","-"*3)
+print("Foundation English",":",feScore)
+print("General Business",":",gbScore)
+print("Introduction to Computer Systems",":",icsScore)
+print("Computer Programming",":",cpScore)
+print("-"*18)
