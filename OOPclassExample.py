139d09c850b8a722382c1b23921ac620ad30a7cb
Jirapongs55 <nut36.jirapong@hotmail.com>
2021-08-02 15:15:49 +0700

    OOP Class_HW

---
+++ OOPclassExample.py
@@ -0,0 +1,43 @@
+class Customer:
+    firstName = ""
+    lastName = ""
+    age = 0
+    def addCart(self):
+        print("Added product to",self.firstName,self.lastName,"'s cart")
+
+customer1 = Customer()
+customer1.firstName = "Jirapong"
+customer1.lastName = "S"
+customer1.addCart()
+
+customer2 = Customer()
+customer2.firstName = "Anan"
+customer2.lastName = "O"
+customer2.addCart()
+
+customer3 = Customer()
+customer3.firstName = "Napat"
+customer3.lastName = "K"
+customer3.addCart()
+
+customer4 = Customer()
+customer4.firstName = "Thanacarn"
+customer4.lastName = "M"
+customer4.addCart()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
